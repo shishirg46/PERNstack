e520f51bd0ffcb14de919875edeeb5a59a3e99f2
shishirg46 <shishirg46@gmail.com>
2025-02-27 09:13:58 +0545

    add test for persist user

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { HttpError } from 'http-errors'
 import logger from './config/logger'
 import authRouter from './routes/auth'
 const app = express()
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello world')
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,6 +1,26 @@
 import request from 'supertest'
 import app from '../../src/app'
+import { DataSource } from 'typeorm'
+import { AppDataSource } from '../../src/config/data-source'
+import { User } from '../../src/entity/User'
+import truncateTables from '../utils/index'
 describe('POST auth/register', () => {
+    let connection: DataSource
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+
+    beforeEach(async () => {
+        // database truncate
+        await truncateTables(connection)
+    })
+
+    afterAll(async () => {
+        if (connection) {
+            await connection.destroy() // ✅ Call destroy only if connection exists
+        }
+    })
     describe('given all feilds', () => {
         it('should return 201 status code', async () => {
             const userData = {
@@ -33,6 +53,7 @@ describe('POST auth/register', () => {
                 (response.headers as Record<string, string>)['content-type'],
             ).toEqual(expect.stringContaining('json'))
         })
+
         it('should persist the user in the database', async () => {
             const userData = {
                 firstName: 'Rakesh',
@@ -44,6 +65,14 @@ describe('POST auth/register', () => {
             const response = await request(app)
                 .post('/auth/register')
                 .send(userData)
+
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            expect(users).toHaveLength(1)
+            expect(users[0].firstName).toBe(userData.firstName)
+            expect(users[0].lastName).toBe(userData.lastName)
+            expect(users[0].email).toBe(userData.email)
+            expect(users[0].password).toBe(userData.password)
         })
     })
     describe('fields are missing', () => {
